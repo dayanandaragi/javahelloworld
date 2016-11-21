eda2dac3c71f93e6ec0b244dfd997185f3ccf4f9
dayanandaragi <dayanandaragi@gmail.com>
2016-11-21 11:04:42 +0530

    removed few lines

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -5,8 +5,5 @@ public class JavaHelloWorld
 		System.out.println("Java Hello World!");
 		System.out.println("Hello Again");
 		System.out.println("Hello Ragi.. u automated build");
-		System.out.println("trigger auto build by url");
-		System.out.println("DeepaSujatha hatr pa");
-		System.out.println("Happy birthday Dhanya Man!");
 	}
 }
